fdf027ccc7ed093379c4ca5939b4a822e94d5d2f
daniel <s00236328@atu.ie>
2024-10-23 16:55:06 +0100

    test

---
+++ Week1/Source/Week1/Week1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Week1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Week1, "Week1" );
